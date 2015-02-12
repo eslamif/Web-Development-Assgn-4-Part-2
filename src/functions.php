dbad12341ff7a9a79e29a1fee08343d2dac0232c
Frank Eslami <FEslami@WB-DAS-33181.ca.epa.local>
2015-02-12 12:44:49 -0800

    Segregated functions to separate file. HTTP request not working

---
+++ src/functions.php
@@ -0,0 +1,123 @@
+<?php 
+/*--------------- PHP FUNCTION DEFINITIONS ---------------*/
+function connectToSql($http) {
+	//Set database variables
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "root";
+	$dbname = "test";
+	
+	//Connect to MySQL Server
+	$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+	if($mysqli->connect_errno) {
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") ". 
+		$mysqli->connect_error;
+	}
+	return $mysqli;
+}
+
+function setSql($http, $mysqli) {
+	//Variables to set
+	$name = $http['name'];
+	$category = $http['category'];
+	$length = $http['length'];
+	
+	//Prepared Statement - prepare
+	if (!($stmt = $mysqli->prepare("INSERT INTO test(name, category, length) VALUES (?, ?, ?)"))) {
+		 echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}	
+	
+	//Prepared Statement - bind and execute 
+	if (!$stmt->bind_param('ssi', $name, $category, $length)) {
+		echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}	
+	
+	if (!$stmt->execute()) {
+		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+	
+	$stmt->close();		//close statement
+}
+
+
+function getSql($mysqli) {
+	$out_name = NULL;
+	$out_category = NULL;
+	$out_length = NULL;
+	
+	//Prepared Statement - prepare
+	if (!($stmt = $mysqli->prepare("SELECT name, category, length FROM test"))) {
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	
+	if (!$stmt->execute()) {
+		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+
+	//Bind results
+	if (!$stmt->bind_result($out_name, $out_category, $out_length)) {
+	    echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+	
+	/*
+	//Display results in table
+	echo "<table>";
+		echo "<tr> <td colspan=2>Video Listing</td>
+			<td colspan=2><form action=http://localhost/myhost-exemple/cs290-ass4-p2/src/index.php method=GET>
+				<button type=submit name=video value=true>Delete All</form></td>";
+		echo "<tr> <td>Name</td> <td>Category</td> <td>Length</td> <td>Status</td> </tr>";
+		while($stmt->fetch()) {
+			echo "<tr>";
+			echo "<td>".$out_name."</td> <td>".$out_category."</td> <td>".$out_length."</td>";
+			echo "</tr>";
+		}
+	echo "</table>";
+	
+	//Delete All Videos
+	if(isset($_GET['video']) && $_GET['video'] == TRUE) {
+		$mysqli->query("DELETE FROM test");
+	}
+	*/
+	
+	$stmt->close();		//close statement
+}
+?>
+
+
+<!-- ----------------- JAVASCRIPT FUNCTION DEFINITIONS ------------------- -->
+<script>
+function displayVideoList() {
+	var videoList = httpRequest();
+
+}
+
+
+function httpRequest() {
+	var http;
+	if(window.XMLhttp) {										//Browsers other than internet explorer
+		http = new XMLhttp();
+	}
+	else if(window.ActiveXObject) {								//internet explore browser
+		http = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	if(!http) {
+		throw 'Unable to create http.';
+	}	
+
+	http.onreadystatechange = function() {
+		if(http.readyState == 4) {
+			var resultParsed = JSON.parse(http.responseText);	//save results
+		}
+	}
+	http.open("GET", "URL HERE", true);
+	http.send();
+	
+	return resultParsed;
+}
+
+
+</script>
+
+
+
--- src/index.php
+++ src/index.php
@@ -4,9 +4,12 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header('Content-Type: text/html');
 
+include "functions.php";
+
 
 /*--------------- MAIN PROGRAM ---------------*/
 ?>
+
 <!--  ADD VIDEO INTERFACE -->
 <form action="http://localhost/myhost-exemple/cs290-ass4-p2/src/index.php" method="POST">
 <fieldset>
@@ -29,6 +32,8 @@ header('Content-Type: text/html');
 </fieldset>	
 </form>	
 
+<div id="videoList">Video List from Database goes here!</div>
+
 
 <?php 
 $mysqli = connectToSql($_POST);		//connect to MySQL
@@ -37,93 +42,14 @@ $mysqli = connectToSql($_POST);		//connect to MySQL
 if(isset($_POST['action']) && $_POST['action'] == "addVideo")
 	setSql($_POST, $mysqli);
 
-getSql($mysqli);					//get video list from MySQL
-//displaySql();						//display video list to user
-$mysqli->close();					//close MySQL connection
+getSql($mysqli);									//get video list from MySQL
+echo "<script> displayVideoList(); </script>";		//display video list to user
+$mysqli->close();									//close MySQL connection
 
 
 
 
-/*--------------- FUNCTION DEFINITIONS ---------------*/
-function connectToSql($http) {
-	//Set database variables
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpass = "root";
-	$dbname = "test";
-	
-	//Connect to MySQL Server
-	$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-	if($mysqli->connect_errno) {
-		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") ". 
-		$mysqli->connect_error;
-	}
-	return $mysqli;
-}
 
-function setSql($http, $mysqli) {
-	//Variables to set
-	$name = $http['name'];
-	$category = $http['category'];
-	$length = $http['length'];
-	
-	//Prepared Statement - prepare
-	if (!($stmt = $mysqli->prepare("INSERT INTO test(name, category, length) VALUES (?, ?, ?)"))) {
-		 echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	}	
-	
-	//Prepared Statement - bind and execute 
-	if (!$stmt->bind_param('ssi', $name, $category, $length)) {
-		echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-	}	
-	
-	if (!$stmt->execute()) {
-		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
-	
-	$stmt->close();		//close statement
-}
-
-
-function getSql($mysqli) {
-	$out_name = NULL;
-	$out_category = NULL;
-	$out_length = NULL;
-	
-	//Prepared Statement - prepare
-	if (!($stmt = $mysqli->prepare("SELECT name, category, length FROM test"))) {
-		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	}
-	
-	if (!$stmt->execute()) {
-		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
-
-	//Bind results
-	if (!$stmt->bind_result($out_name, $out_category, $out_length)) {
-	    echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
-	
-	//Display results in table
-	echo "<table>";
-		echo "<tr> <td colspan=2>Video Listing</td>
-			<td colspan=2><form action=http://localhost/myhost-exemple/cs290-ass4-p2/src/index.php method=GET>
-				<button type=submit name=video value=true>Delete All</form></td>";
-		echo "<tr> <td>Name</td> <td>Category</td> <td>Length</td> <td>Status</td> </tr>";
-		while($stmt->fetch()) {
-			echo "<tr>";
-			echo "<td>".$out_name."</td> <td>".$out_category."</td> <td>".$out_length."</td>";
-			echo "</tr>";
-		}
-	echo "</table>";
-	
-	//Delete All Videos
-	if(isset($_GET['video']) && $_GET['video'] == TRUE) {
-		$mysqli->query("DELETE FROM test");
-	}
-		
-	$stmt->close();		//close statement
-}
 
 ?>
 
