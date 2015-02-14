7a146f3c0dbe00ebb4db8c78e57c10453ea3e6ae
Frank Eslami <eslamif@oregonstate.edu>
2015-02-14 14:02:34 -0800

    Server returns JSON object. Next - JS needs to display object in table

--- newtrial/database.php
+++ newtrial/database.php
@@ -2,13 +2,38 @@
 //Enable error detection
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-header('Content-Type: text/plain');
+header('Content-Type: text/html');
 
+
+
+
+
+
+
+if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'getVideo') {
+	/*
+	$mysqli = connectToSql($_REQUEST);
+	$result = getSql($mysqli);
+	*/
+	
+	$arr = array(
+		'name' => 'Mike1',
+		'name' => 'Mike2',
+		'age' => 5
+	);
+	
+	$arr = json_encode($arr);
+	echo $arr;
+	
+	
+}
 
 
 if(isset($_REQUEST['action'])  && $_REQUEST['action'] == 'addVideo') {
 	$mysqli = connectToSql($_REQUEST);
 	setSql($_REQUEST, $mysqli);
+	$return = $_REQUEST['name']; 
+	echo $return;
 }
 
 
@@ -30,6 +55,7 @@ function connectToSql($http) {
 	}
 	return $mysqli;
 }	
+
 
 function setSql($http, $mysqli) {
 	//Variables to set
@@ -53,4 +79,51 @@ function setSql($http, $mysqli) {
 	
 	$stmt->close();		//close statement
 }
+
+
+function getSql($mysqli) {
+	$out_name = NULL;
+	//$out_category = NULL;
+	//$out_length = NULL;
+	
+	//Prepared Statement - prepare
+	if (!($stmt = $mysqli->prepare("SELECT name FROM test"))) {
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	
+	//Prepared Statement - execute
+	if (!$stmt->execute()) {
+		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+
+	//Bind results
+	if (!$stmt->bind_result($out_name)) {
+	    echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+	
+	//Store result in array
+	$stmt->fetch();
+	
+	$stmt->close();	
+	return $out_name;
+}
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- newtrial/jsFunctions.js
+++ newtrial/jsFunctions.js
@@ -1,6 +1,33 @@
+function getVideoList() {
+	var http;
+	if(window.XMLHttpRequest) {					//Browsers other than internet explorer
+		http = new XMLHttpRequest();
+	}
+	else if(window.ActiveXObject) {				//internet explore browser
+		http = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	if(!http) {
+		throw 'Unable to create HttpRequest.';
+	}	
+
+	http.onreadystatechange = function() {
+		if(http.readyState == 4) {
+			//var resultParsed = JSON.parse(http.responseText);	//save results
+			//var obj = http.responseText;
+			
+			//var obj = JSON.parse(http.responseText);
+			//document.getElementById("testing").innerText = obj;
+			document.getElementById("testing").innerText = http.responseText;
+		}
+	}
+	http.open("GET", "http://localhost/myhost-exemple/cs290-ass4-p2/newtrial/database.php?action=getVideo", true);
+	http.send();
+}
+
+
 function httpAddVideo() {
 	var nameInput = document.getElementById("name").value;
-	console.log(nameInput);
 	
 	var http;
 	if(window.XMLHttpRequest) {					//Browsers other than internet explorer
@@ -17,7 +44,7 @@ function httpAddVideo() {
 	http.onreadystatechange = function() {
 		if(http.readyState == 4) {
 			//var resultParsed = JSON.parse(http.responseText);	//save results
-			document.getElementById("videoList").innerText = http.responseText;
+			document.getElementById("videoList").innerText = http.responseText + ' was added to the database.';
 		}
 	}
 	http.open("GET", "http://localhost/myhost-exemple/cs290-ass4-p2/newtrial/database.php?action=addVideo&name=" + nameInput, true);
